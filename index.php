1b55e3023bebee952ab343e9e3258fcf2000afc9
Favio Alvarado <70043499+favio22@users.noreply.github.com>
2023-05-03 13:31:00 -0500

    Update index.php
    Actualiacion de temarios

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ document.getElementById("elnombre").innerHTML="FavioA";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='http://congresoutlvte.org/OrtizNixon/'>Nixon Ortiz</a></p> ";
  </script>
  
 <?php
