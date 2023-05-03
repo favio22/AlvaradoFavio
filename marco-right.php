574bd88806c53d86822b129d0106b02fef3fb9f0
favio22 <favio.alvarado.fernandez@utelvt.edu.ec>
2023-05-03 08:56:36 -0500

    ACTUALIZACION DE FOTO

--- marco-right.php
+++ marco-right.php
@@ -5,7 +5,7 @@
     <div style="margin-top: 80px; ">
         <div style=" width: 10vw;  margin:auto ; padding:1px; border-radius:50%; " >
     				<div class="w3-card-4" style="margin: auto;   width:100%; border-top:5px solid green;  border-bottom:5px solid green; padding:0.5em; height:200px;border-radius:50%;">
-         				<img id="foto" style="border-radius:50%; width:100%; height:100%;" src="http://educaysoft.org/images/coordinador-MTI.png" alt="El coordinador del programa" >
+         				<img id="foto" style="border-radius:50%; width:100%; height:100%;" src="https://github.com/favio22/AlvaradoFavio/blob/main/images/FAVIO-A.png?raw=true" alt="El coordinador del programa" >
     				</div>
 <center><span id="elnombre" style="padding:5px; border:1px solid gray; margin:0; background-color: green; color:white; text-shadow: 1px 1px black; ">Kevin Aguilar</span></center>
 <hr>
