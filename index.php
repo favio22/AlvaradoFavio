4816591211fff2d1974c213aa97c9e336f525a07
favio22 <favio.alvarado.fernandez@utelvt.edu.ec>
2023-05-03 07:37:51 -0500

    actualizacion de la  informacion de perfil

--- index.php
+++ index.php
@@ -24,9 +24,9 @@ include("marco-right.php");
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy favio Alvarado <b> Favio Alvarado </b>, estudiante esmeraldeño que estudia ingereria en sistema en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
-<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
+                   <p class="eys-parrafo">Hola, yo soy favio Alvarado <b> Favio Alvarado </b>, estudiante esmeraldeño que estudia ingereria en TIC´S en la Universidad Tecnica Luis Vargas Torres, e recivido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web,base de datos, Proyectos informáticos entre otras; pero la cátedra que mas me llama la atencion es base de datos que de dicta en tercero y cuarto  ciclo de la carrera de Tecnología de la Información. </p>
+                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por  la tecnología, ya que cada dia la tecnologia evoluciona y tanbien es un campo laboral muy amplio</p>
+<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://es.wikipedia.org/wiki/C_Sharp#:~:text=C%23%20es%20uno%20de%20los%20lenguajes%20de%20programaci%C3%B3n,Java%2C%20aunque%20incluye%20mejoras%20derivadas%20de%20otros%20lenguajes."> C# </a></b>  para este tiempo la UTELVT  contaba con laboratorios de computacion, donde se realizaba las practicas de programacion.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,23 +34,23 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en la unidad Educativa Libertad de Timbre  donde me gradue en la promocion 2018-2019, de la especialidad de Tecnico de Servicios Aplicaciones Informaticas apenas me gradue postule para la univercidad para seguir mis estudios universitarios en la Univercidad Tecnica de Esmeraldas Luis Vargas Torres  donde seguí la carrera de TECNOLOGIA DE LA INFORMACION, gracias a los conocimientos obtenido en el colegio y autopreparacion trabajo mientra estoy estudiando, prestando mis servicios  en servicio tecnico de computadoras, la cual lo hago para ayudarme en mis estudios y asi poder obtener mi titulo en TECNOLOGIAS DE LA INFORMACION</p>
+                   <p class="eys-parrafo">Una vez que obtenga el título de TECNOLOGIA DE LA INFORACION en la mano aplicare a plaza de emppeleo en la ciudad de Esmeraldas para brindarle a la ciudadania esmeraldeña mis concimientos y habilidades.</p>
+<p class="eys-parrafo"> En un futuro tengo pensado sacar una maestria en ciberseguridad en el pais de Rusia .</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+                   <p  class="eys-parrafo">Mi perfil es estudiantil , mi primer programa lo hice en el leguaje C, utilizando el computador de la Unidad Educativa Libertad de Timbre, a lo largo de mi carrera univercitaria he venido programando en varios lenguajes como StarUML, C#, programacion orientada a la web con lenguajes de programacion como php y java.</p> 
+                   <p  class="eys-parrafo">Hoy en día los docente la UTELVT nos incentiva a ir mas halla como desarrollar sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo revista tecnologicas, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
        
 
                    
@@ -62,7 +62,7 @@ include("marco-right.php");
                   </div>
 
                   <div style="width: 40%; float: right; text-align: right;">
-                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                   <a href="aprendiendo-cpp">Programa para aprender Java &#187;</a>
                   </div>
 
                 </div>
